d97a3ce481a4918aa2ee844883bfd7ecaf75cee9
Romain Pouclet <romain.pouclet@gmail.com>
2018-11-09 11:18:42 -0800

    Initial Commit

---
+++ SampleWatchApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleWatchApp
+//
+//  Created by Romain Pouclet on 2018-11-09.
+//  Copyright © 2018 Perfectly-Cooked. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
